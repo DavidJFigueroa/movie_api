e19492901cfd61e46a6450083611e9b182001c1d
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-06-15 16:45:36 +0200

    feat: Endpoints & documentation added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express"),
   morgan = require("morgan"),
   fs = require("fs"),
-  path = require("path");
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
@@ -11,6 +13,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 // log all requests
 
 app.use(morgan("combined", {stream: accessLogStream}));
+app.use(bodyParser.json());
 
 // Serve static files from public folder
 
@@ -33,6 +36,42 @@ app.get("/", (req, res) => {
 
 app.get("/movies", (req, res) => {
   res.json(topMovies);
+});
+
+app.get("/movies/:name", (req, res) => {
+  res.send("Successful GET request returning data of a movie by name");
+});
+
+app.get("/genres/:name", (req, res) => {
+  res.send("Successful GET request returning data of a genre by name");
+});
+
+app.get("/directors/:name", (req, res) => {
+  res.send("Successful GET request returning data of a director by name");
+});
+
+app.post("/users", (req, res) => {
+  res.send("Successful POST request for users to register");
+});
+
+app.put("/users/:name", (req, res) => {
+  res.send("Successful PUT request for users to update their info");
+});
+
+app.put("/users/:name/favorites", (req, res) => {
+  res.send(
+    "Successful PUT request for users to add movies to their favorite list"
+  );
+});
+
+app.delete("/users/:name/favorites", (req, res) => {
+  res.send(
+    "Successful PUT request for users to remove movies from their favorite list"
+  );
+});
+
+app.delete("/users", (req, res) => {
+  res.send("Successful DELETE request for users to deregister");
 });
 
 // handling errors
